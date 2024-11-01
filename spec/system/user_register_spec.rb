ef46a5c6c9dd7b2c3e2525ab29f087ce853f6dfc
Laís Rodrigues <devlaisrodrigues@gmail.com>
2024-11-01 15:55:35 -0300

    adiciona filtro de busca, bootstrap e refatora testes

--- spec/system/user_register_spec.rb
+++ spec/system/user_register_spec.rb
@@ -3,84 +3,55 @@ require 'rails_helper'
 describe 'Cadastro de Funcionário' do
   it 'com sucesso' do
     # Arrange
-    user = User.create!(
-      name: 'User',
-      email: 'user99913@example.com',
-      last_name: 'Last Name',
-      cpf: '483.556.180-50',
-      password: 'password1234567'
-    )
-
-    establishment = Establishment.create!(
-      name: 'Establishment',
-      social_name: 'Establishment',
-      cnpj: '47.761.353/0001-78',
-      full_address: '123 Main St',
-      city: 'Anytown',
-      state: 'ST',
-      postal_code: '12345',
-      email: 'establishment9992@example.com',
-      phone_number: '1234567890',
-      user_id: user.id
-    )
+    visit root_path
 
     # Act
-    visit new_user_path(establishment_id: establishment.id)
-    expect(page).to have_content('Cadastrar')
+    click_link 'Criar conta'
+
+    expect(page).to have_content('Cadastre-se')
     
-    within('nav') do
-      click_link 'Cadastrar'
-    end
-
-    within('#new_user') do
-      fill_in 'Nome', with: 'João Silva'
-      fill_in 'Sobrenome', with: 'Silva'
-      fill_in 'CPF', with: '483.556.180-50'
-      fill_in 'E-mail', with: 'joao.silva@exemplo.com'
-      fill_in 'Senha', with: 'teste123456789'
-      fill_in 'Confirmar Senha', with: 'teste123456789'
-    end
-
+    fill_in 'Nome', with: 'Jane'
+    fill_in 'Sobrenome', with: 'Doe'
+    fill_in 'CPF', with: '529.982.247-25'
+    fill_in 'E-mail', with: 'jane@doe.com.br'
+    fill_in 'Senha', with: 'password123456'
+    fill_in 'Confirmar Senha', with: 'password123456'
+    
     click_button 'Cadastrar'
 
     # Assert
-    expect(page).to have_content('Login efetuado com sucesso.')
-    expect(User.last.name).to eq('João Silva')
-    expect(User.last.email).to eq('joao.silva@exemplo.com')
+    expect(page).to have_content('Login efetuado com sucesso')
+    expect(page).to have_content('jane@doe.com.br')
+    expect(page).to have_link('Sair')
   end
 
   it 'e vê mensagens de erro quando dados são inválidos' do
     # Arrange
-    user = User.create!(
-      name: 'User',
-      email: 'user663@example.com',
-      last_name: 'Last Name',
-      cpf: '483.556.180-50',
-      password: 'password1234567'
-    )
+    visit root_path
 
-    establishment = Establishment.create!(
-      name: 'Establishment',
-      social_name: 'Establishment',
-      cnpj: '16.424.880/0001-63',
-      full_address: '123 Main St',
-      city: 'Anytown',
-      state: 'ST',
-      postal_code: '12345',
-      email: 'establishment662@example.com',
-      phone_number: '1234567890',
-      user_id: user.id
-    )
-    
     # Act
-    visit new_user_registration_path(establishment_id: establishment.id)
-    within('nav') do
-      click_link 'Cadastrar'
+    within 'nav' do
+      click_link 'Criar conta'
     end
+
     click_button 'Cadastrar'
 
     # Assert
-    expect(page).to have_content('Name não pode ficar em branco')
-    expect(page).to have_content('Email não pode ficar em branco')
+    expect(page).to have_content('E-mail não pode ficar em branco')
+    expect(page).to have_content('Nome não pode ficar em branco')
+    expect(page).to have_content('Sobrenome não pode ficar em branco')
+    expect(page).to have_content('CPF não pode ficar em branco')
+    expect(page).to have_content('Cpf inválido')
+
+    fill_in 'Nome', with: 'Jane'
+    fill_in 'Sobrenome', with: 'Doe'
+    fill_in 'CPF', with: '529.982.247-25'
+    fill_in 'E-mail', with: 'jane@doe.com.br'
+    fill_in 'Senha', with: '123'
+    fill_in 'Confirmar Senha', with: '123'
+
+    click_button 'Cadastrar'
+
+    expect(page).to have_content('12 (minimo de caracteres)')
   end
 end 
