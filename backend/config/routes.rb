2610eb49cf76a31d48997bea73c82a95e4f44475
Laís Rodrigues <devlaisrodrigues@gmail.com>
2026-01-19 13:54:43 -0300

    feat: add set_cookie route for authentication
    - Add POST /api/v1/set_cookie endpoint
    - Update routes to use new Web::Establishments namespace

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,44 +1,43 @@
 Rails.application.routes.draw do
-  # Rota temporária para capturar requisições antigas do Devise e redirecionar para API
   post '/users/sign_in', to: 'api/v1/sessions#create'
   
-  root to: 'establishments#index'
+  root to: 'web/establishments/establishments#index'
 
-  resources :establishments do
+  resources :establishments, controller: 'web/establishments/establishments' do
     get 'search', on: :collection
-    resources :working_hours, only: %i[edit update]
-    resources :dishes do
-      resources :portions
+    resources :working_hours, controller: 'web/establishments/working_hours', only: %i[edit update]
+    resources :dishes, controller: 'web/establishments/dishes' do
+      resources :portions, controller: 'web/establishments/portions'
       member do
         patch :toggle_status
       end
     end
 
-    resources :drinks do
-      resources :portions
+    resources :drinks, controller: 'web/establishments/drinks' do
+      resources :portions, controller: 'web/establishments/portions'
       member do
         patch :toggle_status
       end
     end
 
-    resources :portions
-    resources :tags
+    resources :portions, controller: 'web/establishments/portions'
+    resources :tags, controller: 'web/establishments/tags'
 
-    resources :menus do
-      resources :menu_items
+    resources :menus, controller: 'web/establishments/menus' do
+      resources :menu_items, controller: 'web/establishments/menu_items'
     end
 
-    resources :orders do
+    resources :orders, controller: 'web/establishments/orders' do
       member do
-        get :add_item, to: 'orders#add_item'
-        post :save_item, to: 'orders#save_item'
-        delete :remove_item, to: 'orders#remove_item'
-        patch :change_status, to: 'orders#change_status'
-        patch :cancel, to: 'orders#cancel'
+        get :add_item, to: 'web/establishments/orders#add_item'
+        post :save_item, to: 'web/establishments/orders#save_item'
+        delete :remove_item, to: 'web/establishments/orders#remove_item'
+        patch :change_status, to: 'web/establishments/orders#change_status'
+        patch :cancel, to: 'web/establishments/orders#cancel'
       end
     end
 
-    resources :employee_invitations
+    resources :employee_invitations, controller: 'web/establishments/employee_invitations'
   end
 
   namespace :api do
@@ -62,6 +61,7 @@ Rails.application.routes.draw do
       post '/sign_in', to: 'sessions#create'
       delete '/sign_out', to: 'sessions#destroy'
       get '/is_signed_in', to: 'sessions#is_signed_in?'
+      post '/set_cookie', to: 'sessions#set_cookie'
     end
   end
 end
