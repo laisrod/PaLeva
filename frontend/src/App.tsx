94ae8740d191776b81208271619b4c33e73e105e
Laís Rodrigues <devlaisrodrigues@gmail.com>
2026-03-03 09:49:19 -0300

    refactor(auth): centralize session state with AuthProvider context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,38 @@
 import { BrowserRouter } from 'react-router-dom'
-import AuthListener from './components/AuthListener'
 import Navbar from './components/Navbar'
 import AppRoutes from './components/AppRoutes'
 import NotificationToast from './shared/components/NotificationToast'
 import { useNotifications } from './shared/hooks/useNotifications'
+import { AuthProvider } from './shared/hooks/useAuth'
 import { CartProvider } from './client/features/cart/contexts/CartContext'
 import './css/shared/App.css'
 
-function App() {
-  // Inicializar sistema de notificações
+function AppContent() {
+  // Inicializar sistema de notificações quando auth estiver disponível no contexto
   useNotifications()
 
   return (
+    <>
+      <Navbar>
+        <AppRoutes />
+      </Navbar>
+      <NotificationToast />
+    </>
+  )
+}
+
+function App() {
+  return (
     <div className="App">
       <BrowserRouter>
-        <CartProvider>
-          <AuthListener />
-          <Navbar>
-            <AppRoutes />
-          </Navbar>
-          <NotificationToast />
-        </CartProvider>
+        <AuthProvider>
+          <CartProvider>
+            <AppContent />
+          </CartProvider>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   )
 }
 
 export default App
-
