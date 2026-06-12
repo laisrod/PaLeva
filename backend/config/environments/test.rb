316dd31d6e29843a0589b8a6026e97b01902908d
Laís Rodrigues <devlaisrodrigues@gmail.com>
2026-06-12 08:58:24 -0300

    fix(test): configure Sprockets MemoryStore to avoid permission error

--- backend/config/environments/test.rb
+++ backend/config/environments/test.rb
@@ -50,4 +50,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.assets.configure do |env|
+    env.cache = ActiveSupport::Cache::MemoryStore.new
+  end
 end
